957475e9789fe051f2cd918dc1c265ef09594f1a
tozydev <tozydev@nguyenthanhtan.id.vn>
2024-06-02 16:13:08 +0700

    deps: upgrade dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 
 plugins {
-    id("io.github.grassmc.waddle") version "1.1.0"
+    id("io.github.grassmc.waddle") version "2.0.0"
 }
 
 rootProject.name = "paper-plugin-template"
